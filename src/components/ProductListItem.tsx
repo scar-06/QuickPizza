2f8aff1108525102ecd9238f360f99f774e319dc
Oscar <nwanzeoscar@gmail.com>
2024-06-23 18:30:20 +0100

    created ProductListItem component

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, Image } from "react-native";
+
+import EditScreenInfo from "@/src/components/EditScreenInfo";
+import { Text, View } from "@/src/components/Themed";
+import Colors from "@/src/constants/Colors";
+import products from "@/assets/data/products";
+
+const ProductListItem = ({ product }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+  },
+
+  title: { fontSize: 18, fontWeight: "600", marginVertical: 10 },
+
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
