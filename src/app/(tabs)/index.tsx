a7d29f06c4497adc2ece24851322717f33ad333c
Oscar <nwanzeoscar@gmail.com>
2024-06-24 03:39:51 +0100

    setting of header titles

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,17 +1,5 @@
-import { FlatList } from "react-native";
+import { Redirect } from "expo-router";
 
-import { Text, View } from "@/src/components/Themed";
-import products from "@/assets/data/products";
-import ProductListItem from "@/src/components/ProductListItem";
-
-export default function MenuScreen() {
-  return (
-    <FlatList
-      data={products}
-      renderItem={({ item }) => <ProductListItem product={item} />}
-      numColumns={2}
-      contentContainerStyle={{ gap: 10, padding: 10 }}
-      columnWrapperStyle={{ gap: 10 }}
-    />
-  );
+export default function TabIndex() {
+  return <Redirect href={"/menu/"} />;
 }
--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,12 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { ColorProperties } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
-import { useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
   return (
     <View>
+      <Stack.Screen options={{ title: "Details: " + id }} />
+
       <Text style={{ color: "green", fontSize: 20 }}>
         ProductDetailsScreen for id: {id}
       </Text>
