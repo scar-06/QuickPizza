33a5c7102e0363176c86577db4319a21e6ea818a
Oscar <nwanzeoscar@gmail.com>
2024-06-30 18:13:06 +0100

    initiated auth via supabase

--- src/app/(auth)/signin.tsx
+++ src/app/(auth)/signin.tsx
@@ -1,12 +1,29 @@
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import { View, Text, TextInput, StyleSheet, Alert } from "react-native";
 import React, { useState } from "react";
 import Button from "../../components/Button";
 import Colors from "../../constants/Colors";
 import { Link, Stack } from "expo-router";
+import { supabase } from "@/src/lib/supabase";
 
 const SignInScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const [loading, setLoading] = useState(false); //to "track loading state", in order to avoid signUpWithEmail be called multiple times
+
+  async function signInWithEmail() {
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      Alert.alert(error.message);
+    }
+
+    setLoading(false);
+  }
 
   return (
     <View style={styles.container}>
@@ -29,7 +46,11 @@ const SignInScreen = () => {
         secureTextEntry
       />
 
-      <Button text="Sign in" />
+      <Button
+        onPress={signInWithEmail}
+        disabled={loading}
+        text={loading ? "Signing in..." : "Sign in"}
+      />
       <Link href="/signup" style={styles.textButton}>
         Create an account
       </Link>
